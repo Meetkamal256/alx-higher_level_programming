83e6b1ab82e6516ff379468040da8bd6805c66cd
Kamaluddeen Muhammad <meetkamal256@gmail.com>
2022-09-29 04:50:48 +0100

    push

--- 0x06-python-classes/103-main.py
+++
@@ -1,3 +0,0 @@
-Magic = __import__('103-magic_class').MagicClass
-
-s_5 = Magic(3.32)
--- 0x07-python-test_driven_development/compile.sh
+++
@@ -1,1 +0,0 @@
-gcc -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I/usr/include/python3.4 102-python.c
